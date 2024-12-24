fd5e504ccf951ce11bfd59a3f6d4392ff714734f
306 <306@306NEW-12>
2024-12-24 09:41:37 +0300

    Добавьте файлы проекта.

---
+++ StudentConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using StudentLibrary;
+
+namespace StudentConsoleApp
+{
+    class Programm { 
+    static void Main(string[] args)
+        {
+            Group group = new Group("36ИС", "1-МОК");
+            group.addStudent(new Student("Козырев Максим", 3.5)); 
+            group.addStudent(new Student("Ватаву Степан", 4.9)); //Выполнил Ватаву Степан
+            group.addStudent(new Student("Боровых Виван", 4.7));
+            group.addStudent(new Student("Иван Иванов", 3.0)); 
+            group.addStudent(new Student("Сабуро Арасака", 4.3));
+            group.ShowAll();
+        }
+    }
+       
+    
+}
---
+++ StudentConsoleApp/StudentLibrary.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentLibrary
+{
+    public class Student
+    {
+        public string Name { get; set; } // Выполнил Ватаву
+        public double Ocenka { get; set; }
+        public Student(String name, double ocenka) {
+            this.Name = name; //ВЫполнил Ватаву
+            this.Ocenka = ocenka;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Имя: " + Name + " Оценка: " + Ocenka);
+        }
+    }
+    public class Group{
+        public string Name { get; set; }
+        public string College { get; set;}
+        public List<Student> Students { get; set; } = new List<Student>();
+        public Group(String name, String college) { 
+            this.Name=name;
+            this.College=college;
+        }
+        public void addStudent(Student student)
+        {
+            Students.Add(student);
+        }
+        public void ShowAll() {
+            Console.WriteLine($"Группа: {Name}, Колледжа: {College}");
+            foreach(Student student in Students)
+            {
+                student.Show();
+            }
+        }
+    }
+}
